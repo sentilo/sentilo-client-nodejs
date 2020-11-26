2ae7ea8b2bf9ec292f2f9395562534e219e9869f
kcech <kcech@opentrends.net>
2020-11-26 13:55:38 +0100

    Options now permit Sentilo host with https protocol

--- src/ServicesConfiguration.js
+++ src/ServicesConfiguration.js
@@ -29,7 +29,7 @@
 var utils = require('./utils/SentiloUtils');
 
 var defaultServicesConfig = {
-	host : 'localhost',
+	host : 'http://localhost:8081',
     providerId : 'testApp_provider',
     component : 'TEST_COMPONENT_001',
     sensor : 'TEST_SENSOR_001'
